bb80774390f35d6495194f7045beb714f6565d61
csamatov <csamatov.devops@gmail.com>
2022-07-15 15:27:00 -0500

    taget-

--- main.tf
+++ main.tf
@@ -16,8 +16,9 @@ provider "aws" {
   region = var.aws_region
 }
 
+#The bucket module uses this resource to assign a randomized name to the S3 bucket.
 resource "random_pet" "bucket_name" {
-  length    = 3
+  length    = 5
   separator = "-"
   prefix    = "learning"
 }
@@ -35,15 +36,17 @@ resource "random_pet" "object_names" {
 
   length    = 5
   separator = "_"
-  prefix    = "learning"
+#  prefix    = "learning"
 }
 
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_object
 resource "aws_s3_bucket_object" "objects" {
   count = 4
 
   acl          = "public-read"
   key          = "${random_pet.object_names[count.index].id}.txt"
   bucket       = module.s3_bucket.s3_bucket_id
-  content      = "Example object #${count.index}"
+  #content      = "Example object #${count.index}"
+  content      = "Bucket object #${count.index}"
   content_type = "text/plain"
 }
